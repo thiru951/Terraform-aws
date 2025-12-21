2ab407df728d063bbeda93fb4b95d30c0cd02e64
thiru951 <unvantedsw@gmail.com>
2025-12-21 11:06:08 +0000

    t

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-main
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Thiru-Terraform-EC2"
+  }
+}

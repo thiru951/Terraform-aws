de589fd1bc8571c5953f3da337ec99455ab18766
thiru951 <unvantedsw@gmail.com>
2025-12-21 11:20:01 +0000

    t

--- main.tf
+++ main.tf
@@ -2,11 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "demo" {
-  ami           = "ami-0e86e20dae9224db8"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
     Name = "Thiru-Terraform-EC2"
   }
 }
+

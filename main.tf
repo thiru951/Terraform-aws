8a23db84a6d3227618e64955a6ce8b37d9684239
thiru951 <unvantedsw@gmail.com>
2025-12-21 12:49:24 +0000

    Final working Terraform workflow

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-1"
 }
 
 data "aws_ami" "amazon_linux" {

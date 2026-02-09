874d5f5f4c3cb8f7f1d3c53e5f11dec3e275e26d
thiru951 <unvantedsw@gmail.com>
2026-02-09 06:38:47 +0000

    updates3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "thiru-terraform-state-devops"
+    bucket         = "thiru-terraform-state-devops1"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
